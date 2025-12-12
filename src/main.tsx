5464d15e229dfc3aa467ae818f387e6fc4377653
David Goss <david@davidgoss.co>
2025-12-12 17:41:02 +0000

    move tagging out, add tag for compression

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import './index.css'
 
-import type { Envelope } from '@cucumber/messages'
 import * as Sentry from '@sentry/react'
-import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ReactGA from 'react-ga4'
@@ -26,20 +25,6 @@ const queryClient = new QueryClient({
       retry: 1,
     },
   },
-  queryCache: new QueryCache({
-    onSuccess: (data: unknown) => {
-      const envelopes = data as ReadonlyArray<Envelope>
-      const meta = envelopes?.find((e) => e.meta)?.meta
-      if (meta) {
-        Sentry.setTag('meta_os_name', meta.os.name)
-        Sentry.setTag('meta_os_version', meta.os.version)
-        Sentry.setTag('meta_runtime_name', meta.runtime.name)
-        Sentry.setTag('meta_runtime_version', meta.runtime.version)
-        Sentry.setTag('meta_implementation_name', meta.implementation.name)
-        Sentry.setTag('meta_implementation_version', meta.implementation.version)
-      }
-    },
-  }),
 })
 
 createRoot(document.getElementById('root')!, {
@@ -53,5 +38,5 @@ createRoot(document.getElementById('root')!, {
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 )
